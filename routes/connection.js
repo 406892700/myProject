6f6ee85f0053c25517e11958e9ed79df8647bd1e
徐怀远 <406892700@qq.com>
2015-12-29 18:33:32 +0800

    服务器架设好了

--- routes/connection.js
+++ routes/connection.js
@@ -4,7 +4,7 @@
 module.exports = function(app){
     var mysql      = require('mysql');
     var  connection = mysql.createConnection({
-        host     : 'localhost',
+        host     : '121.42.211.96',
         user     : 'root',
         password : '123456',
         database : 'angualr'
